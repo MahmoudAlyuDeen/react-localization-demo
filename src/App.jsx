8ef6b86d489e0d7208268f290bb71f79144667e3
mahmoud <mahmoudalyudeen@gmail.com>
2018-01-13 14:40:31 +0200

    -feature: language switching

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React, { PureComponent } from "react";
+import {
+  toggleLanguage,
+  withDirectionStyle,
+  withLanguage,
+} from "./redux/language/language";
+import { getString } from "./resources/strings";
+import { withDispatch } from "./redux/withDispatch";
 
+@withLanguage
+@withDispatch
+@withDirectionStyle
 export default class App extends PureComponent {
   render() {
-    return <div>App renders successfully</div>;
+    const { language, dispatch, directionStyle } = this.props;
+    return (
+      <div style={directionStyle}>
+        {getString("HELLO_WORLD", language)}
+        <br />
+        <button onClick={() => dispatch(toggleLanguage())}>
+          Switch language
+        </button>
+      </div>
+    );
   }
 }
---
+++ src/resources/strings.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+
+const strings = {
+  ar: {
+    HELLO_WORLD: "Hello World in Arabic",
+  },
+  en: {
+    HELLO_WORLD: "Hello World in English",
+  },
+};
+
+export const getString = (stringId, language) => {
+  if (!language) {
+    console.warn("getString, language is undefined");
+    return "";
+  }
+  if (!strings[language][stringId]) {
+    const alternativeLanguage = language === "en" ? "ar" : "en";
+    if (!strings[alternativeLanguage][stringId]) {
+      console.warn(
+        "getString, string not found in any language. ID: ",
+        stringId,
+      );
+      return "";
+    }
+    console.warn(`getString, string not found in ${language}. ID: `, stringId);
+    return strings[alternativeLanguage][stringId];
+  }
+  return strings[language][stringId];
+};
+
+export const getAttribute = (entity, which, language) => {
+  if (!entity || !entity.attributes) {
+    console.warn("getAttribute, entity is invalid or undefined");
+    return "";
+  }
+  if (!language) {
+    console.warn("getAttribute, language is undefined");
+    return "";
+  }
+  if (!entity.attributes[which + "-" + language]) {
+    const alternativeLanguage = language === "en" ? "ar" : "en";
+    if (!entity.attributes[which + "-" + alternativeLanguage]) {
+      return "";
+    }
+    return entity.attributes[which + "-" + alternativeLanguage];
+  }
+  return entity.attributes[which + "-" + language];
+};
