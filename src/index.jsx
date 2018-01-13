ad40724f708fb0acf9e3a26d4720df5c72875596
mahmoud <mahmoudalyudeen@gmail.com>
2018-01-13 14:10:20 +0200

    -chore: redux setup changes: -setup redux store -configure redux-bees reducer and middleware -configure redux dev tools extension

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = require("./redux/store").configure();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root"),
+);
 registerServiceWorker();
